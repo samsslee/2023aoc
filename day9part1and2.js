71cf0be0d66bd449a8273a7ecd82e7ff34f32fcb
samsslee <150728297+samsslee@users.noreply.github.com>
2023-12-09 18:38:06 -0800

    Create day9part1and2.js

---
+++ day9part1and2.js
@@ -0,0 +1,8 @@
+const fileContent = fs.readFileSync("2309input.txt", 'utf-8');
+let reports = fileContent.split('\r\n')
+reports = reports.map(report => {
+    report = report.split(' ').map((number, index) => [index, number])
+    let f = interpolatingPolynomial(report);
+    return f(report.length) // in part1 use report.length, in part2 use -1
+})
+console.log(reports.reduce((accumulator, currentValue) => {return accumulator + currentValue}, 0))
