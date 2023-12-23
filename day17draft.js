a9d086ef123bab345c991e41a015be4e1fec0342
samsslee <150728297+samsslee@users.noreply.github.com>
2023-12-22 23:44:50 -0800

    Update day17draft.js

--- day17draft.js
+++ day17draft.js
@@ -1,173 +1,109 @@
-let fs = require("fs")
-let city = fs.readFileSync("2317input.txt").toString('UTF8').split('\r\n').map(row => row.split('').map(cell => parseInt(cell, 10)))
-let cityrows = city.length
-let citycols = city[0].length
+const fs = require('fs');
+let city = fs.readFileSync("2317input.txt", 'utf-8').split('\r\n').map(row => row.split('').map(Number))
+//console.log(city)
 
-class MinHeap {
-    constructor() {
-      this.heap = [];
-      this.nodeMap = {};
-    }
-    getKey(node) {
-        return `${node.x}_${node.y}_${node.d.r}_${node.d.c}_${node.s}`; // Modify this based on your node representation
-    }
+let queue = []
+let end = false
+let sptSet = new Set()
 
-    insert(value, node) {
-      this.heap.push({ value, node });
-      this.nodeMap[this.getKey(node)] = this.heap.length - 1; // Update mapping
-      this.bubbleUp();
-    }
-  
-    extractMin() {
-      if (this.heap.length === 0) {
-        return null;
-      }
-      if (this.heap.length === 1) {
-        return this.heap.pop();
-      }
-  
-      const min = this.heap[0];
-      this.heap[0] = this.heap.pop();
-      this.heapify(0);
-  
-      return min;
-    }
-  
-    bubbleUp() {
-      let currentIndex = this.heap.length - 1;
-  
-      while (currentIndex > 0) {
-        const parentIndex = Math.floor((currentIndex - 1) / 2);
-        if (this.heap[currentIndex].value < this.heap[parentIndex].value) {
-          [this.heap[currentIndex], this.heap[parentIndex]] = [this.heap[parentIndex], this.heap[currentIndex]];
-          currentIndex = parentIndex;
+const insertQueue = function(state) {
+    //console.log("state", state)
+    let low = 0
+    let high = queue.length // might have to pass this in later idk
+
+    while (low < high){
+        const mid = Math.floor((low+high)/2)
+        //console.log(state.d, queue[mid].d)
+        if(state.d > queue[mid].d){
+            high = mid;
         } else {
-          break;
+          low = mid + 1;
         }
-      }
     }
-  
-    heapify(index) {
-      const leftChildIndex = 2 * index + 1;
-      const rightChildIndex = 2 * index + 2;
-      let smallestIndex = index;
-  
-      if (leftChildIndex < this.heap.length && this.heap[leftChildIndex].value < this.heap[smallestIndex].value) {
-        smallestIndex = leftChildIndex;
-      }
-  
-      if (rightChildIndex < this.heap.length && this.heap[rightChildIndex].value < this.heap[smallestIndex].value) {
-        smallestIndex = rightChildIndex;
-      }
-  
-      if (smallestIndex !== index) {
-        [this.heap[index], this.heap[smallestIndex]] = [this.heap[smallestIndex], this.heap[index]];
-        this.heapify(smallestIndex);
-      }
+
+    queue.splice(low, 0, state);
+    //console.log(queue)
+}
+insertQueue({d: 0, n:{x:0, y:0, p:{x:1,y:0}, s:1}})//distance, coordinates, pointing direction ( 1 2 3 4 = e s w n), step counter
+insertQueue({d: 0, n:{x:0, y:0, p:{x:0,y:1}, s:1}})//east
+
+const buildNeighbor = function(state,p,s){
+    let newx = state.n.x + p.x
+    let newy = state.n.y + p.y
+    let newd = state.d + city[newx][newy]
+
+    return {d:newd, n:{x:newx, y:newy, p:{x:p.x, y:p.y}, s:s}}
+}
+
+const makeNeighbors = function(state){
+    let neighbors = []
+    let inbounds = {
+        '{"x":1,"y":0}': (x,y) => x + 1 < city.length,
+        '{"x":-1,"y":0}': (x,y) => x - 1 >= 0,
+        '{"x":0,"y":1}': (x,y) => y + 1 < city[0].length,
+        '{"x":0,"y":-1}': (x,y) => y - 1 >=0
     }
-  
-    getMin(filterCondition) {
-        if (this.heap.length === 0) {
-          return null;
+
+    //should we continue in this current direction?
+    if(state.n.s != 3){ //if we aren't at 3 steps yet
+        if(inbounds[JSON.stringify(state.n.p)](state.n.x, state.n.y)){ //if we are in bounds according to the direction
+            neighbors.push(buildNeighbor(state,state.n.p,state.n.s+1)) //push new distances and coordinates but same pointing direction, add one to the step counter
         }
-    
-        // Filter elements based on the condition
-        const filteredHeap = this.heap.filter(filterCondition);
-    
-        // Find the minimum value in the filtered heap
-        const min = filteredHeap.reduce((minElement, currentElement) => {
-          return currentElement.value < minElement.value ? currentElement : minElement;
-        }, filteredHeap[0]);
-    
-        return min;
     }
-    contains(node) {
-        const key = this.getKey(node);
-        if (this.nodeMap.hasOwnProperty(key)) {
-            const heapIndex = this.nodeMap[key];
-            if (heapIndex != undefined && heapIndex < this.heap.length) {
-                console.log("contains", heapIndex,this.heap[heapIndex])
-                return this.heap[heapIndex].node;
+
+    //if we are going vertically
+    if(state.n.p.x != 0){
+        if(inbounds['{"x":0,"y":1}'](state.n.x, state.n.y)){
+            let neighbor = buildNeighbor(state,{x:0,y:1},1)
+            console.log(sptSet.size, JSON.stringify(neighbor.n), sptSet)
+            console.log("here", sptSet.has(JSON.stringify(neighbor.n)), neighbor.n, sptSet.size)
+            if(!sptSet.has(JSON.stringify(neighbor.n))){
+                console.log("inside", neighbor.n)
+                neighbors.push(neighbor)
             }
         }
-        return null;
-    }
-}
-  
-  
-let distance = new MinHeap()
-let SPTset = new Set();
-const filterCondition = function (element) {
-    return !SPTset.has(element.node);
-};
-
-distance.insert(0, {x:0, y:0, d:{r:1,c:0}, s:0}); //initialize down
-
-let makeNeighbors = function(node){
-    let neighbors = []
-
-    if(node.s < 3){ // for cases where you can still push nodes in the same direction
-        let [new_x,new_y] = [node.x + node.d.r, node.y + node.d.c]
-        let isInBounds = new_x >= 0 && new_x < cityrows && new_y >= 0 && new_y < citycols;
-        if (isInBounds){
-            neighbors.push({x:new_x, y:new_y, d:node.d, s:node.s+1})
-        } 
-    }
-    //for all other cases you can always check the other directions
-    if(node.d.r == 0){ //you are going horizontally, add verticals
-        let new_x= node.x-1
-        console.log("node.x", node.x)
-        //up
-        if (new_x >=0){
-            console.log("newx", new_x)
-            neighbors.push({x:new_x, y:node.y, d:{r:-1,c:0}, s:1})
+        if(inbounds['{"x":0,"y":-1}'](state.n.x, state.n.y)){
+            let neighbor = buildNeighbor(state,{x:0,y:-1},1) //left
+            if(!sptSet.has(JSON.stringify(neighbor.n))){
+                neighbors.push(neighbor)
+            }
         }
-        //down
-        new_x = node.x+1
-        if (new_x < cityrows){
-            neighbors.push({x:new_x, y:node.y, d:{r:1,c:0}, s:1})
+    }else{ //else you're going horizontally so make the verticals
+        if(inbounds['{"x":1,"y":0}'](state.n.x, state.n.y)){
+            let neighbor = buildNeighbor(state,{x:1,y:0},1) //down
+            console.log("neibnode", neighbor.n)
+            if(!sptSet.has(JSON.stringify(neighbor.n))){
+                neighbors.push(neighbor)
+            }
         }
-    } else { // you are going vertically, add horizontals
-        let new_y = node.y-1
-        //left
-        if (new_y >=0){
-            neighbors.push({x:node.x, y:new_y, d:{r:0,c:-1}, s:1})
-        }
-        new_y = node.y+1
-        //right
-        if (new_y < citycols){
-            neighbors.push({x:node.x, y:new_y, d:{r:0,c:1}, s:1})
+        if(inbounds['{"x":-1,"y":0}'](state.n.x, state.n.y)){
+            let neighbor = buildNeighbor(state,{x:-1,y:0},1) //up
+            if(!sptSet.has(JSON.stringify(neighbor.n))){
+                neighbors.push(neighbor)
+            }
         }
     }
+
+    console.log(neighbors)
     return neighbors
 }
-
-while (SPTset.size >= 0){
-    let minDist = distance.getMin(filterCondition);
-    if (!minDist || (minDist.node.x == cityrows-1 && minDist.node.y == citycols-1)) {
-        fs.writeFileSync("output.txt", JSON.stringify(distance));
-        console.log(minDist)
-        break;
-    }
-    console.log("mindist", minDist)
-    SPTset.add(minDist.node);
-    let neighbors = makeNeighbors(minDist.node)
-    for (let neighbor of neighbors){
-        let newDist = minDist.value + city[neighbor.x][neighbor.y]
-        //console.log(minDist.value, city[neighbor.x][neighbor.y] )
-        let existingNode = distance.contains(neighbor)
-        //console.log("existing node", existingNode)
-        if (!existingNode || newDist < existingNode.value) {
-            //console.log("HELLO", existingNode, neighbor)
-            distance.insert(newDist, neighbor);
+let count = 0
+while (count<500){
+    let current = queue.pop()
+    //console.log("current", current)
+    //console.log("queue", queue)
+    console.log(sptSet.has(JSON.stringify(current.n)), sptSet.size)
+    sptSet.add(JSON.stringify(current.n))
+    console.log(sptSet.has(JSON.stringify(current.n)), sptSet.size)
+    //console.log(sptSet)
+    if(current.n.x == city.length && current.n.y == city[0].length){
+        //console.log(current)
+        end = true
+    } else {
+        let neighbors = makeNeighbors(current)
+        for (let neighbor of neighbors){
+            insertQueue(neighbor)
         }
-
-        distance.insert(newDist, neighbor)
     }
-    const SPTarray = Array.from(SPTset);
-    const lastElement = SPTarray[SPTarray.length - 1];
-
-    console.log("last element", lastElement);
+    count++
 }
-
-
