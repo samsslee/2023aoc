4ee0a8041f0ffb6393bdea2b7d94fb3d0aa20cb5
samsslee <150728297+samsslee@users.noreply.github.com>
2024-01-26 10:33:38 -0800

    Update and rename day23draft.js to day23part1.js

--- day23part1.js
+++ day23part1.js
@@ -11,10 +11,10 @@ const readFileLines = filename =>
 let arr = readFileLines('2323input.txt').map(a => a.split(''));
 
 // Printing the response array
-console.log(arr);
+//console.log(arr);
 
 
-let nodes = []//initiate with the source/start node
+let nodes = [[0,1]]//initiate with the source/start node
 
 //find nodes by looking through dots
 const markNodes = function(arr){
@@ -65,7 +65,7 @@ const followPath = function(x,y,dx,dy){
         tile = markedArr[x][y]
         if (x==0 || x==markedArr.length-1){break} //if we've reached either the entry or exit (should only apply in the exit case)
     }
-    console.log(tile,x,y,count)
+    //console.log(tile,x,y,count)
     return [[x,y],count] //this will be the destination node
 }
 
@@ -73,7 +73,7 @@ const findOutgoingNeighbors = function(nx, ny) {
     //assume that all nodes are not on the edges of the map, and assume that none of their neighbors are off the map
     let neighbors = []
     //find all 4 directions
-    console.log(markedArr[nx+1][ny],nx,ny)
+    //console.log(markedArr[nx+1][ny],nx,ny)
     if(markedArr[nx+1][ny] == "v"){neighbors.push([nx+1,ny,1,0])}
     if(markedArr[nx-1][ny] == "^"){neighbors.push([nx-1,ny,-1,0])}
     if(markedArr[nx][ny+1] == ">"){neighbors.push([nx,ny+1,0,1])}
@@ -84,37 +84,66 @@ const findOutgoingNeighbors = function(nx, ny) {
 
 //building all the edges
 let edges = new Map([])
+let nodeDistances = new Map([])
 
 //for the first source
 let start = [0,1] //start at the entry going down
 //find the first node
 let [dest, steps] = followPath(...start,1,0)
-edges.set(start, [{dest: dest, steps: steps}])
+edges.set(JSON.stringify(nodes[0]), [{dest: JSON.stringify(dest), steps: steps}])
+nodeDistances.set(JSON.stringify(nodes[0]), 0)
 
-for (let node of nodes){
-    console.log(node)
-
-    let neighbors = findOutgoingNeighbors(node[0],node[1])
-    console.log(neighbors)
+for (let n = 1; n <nodes.length; n++){
+    //console.log(nodes[n])
+    let neighbors = findOutgoingNeighbors(nodes[n][0],nodes[n][1])
+    let nodeName = JSON.stringify(nodes[n])
+    //console.log(neighbors)
 
     for (let neighbor of neighbors){
         let [dest, steps] = followPath(...neighbor)
-        if (edges.get(node)){
-            edges.get(node).push({dest: dest, steps, steps})
+        if (edges.get(nodeName)){
+            edges.get(nodeName).push({dest: JSON.stringify(dest), steps: steps+1})
         } else{
-            edges.set(node,[{dest: dest, steps, steps}])
+            edges.set(nodeName,[{dest: JSON.stringify(dest), steps: steps+1}])
+            nodeDistances.set(nodeName, 0)
         }
     }
 }
 
+edges.set("source",[{dest:'[0,1]', steps:0}])
+nodeDistances.set(JSON.stringify([arr.length-1,arr[0].length-2]), 0)
 console.log(edges)
+console.log(nodeDistances)
 
 //follow the DAG to get the longest distances for each node
-let nodeDistances = new Map([])
-let current = [0,1] //initiate with source
-let queue = []
 
-while (current[0]!=arr.length){ //while we haven't reached the last row yet
-    let destinations = 
 
+let queue = [["source",0]]
+let count = 0
+
+while (queue.length>0){ //while we haven't reached the last row yet
+    let current = queue.shift()
+    let destinations = edges.get(current[0])
+    //console.log(destinations)
+
+    for (let dest of destinations){
+        //console.log("dest", dest)
+        //console.log(current[1], dest.steps)
+        let totalSteps = current[1] + dest.steps
+        //console.log("total", totalSteps)
+
+        if(nodeDistances.get(dest.dest) < totalSteps){
+            nodeDistances.set(dest.dest, totalSteps)
+        }
+
+        if(dest.dest != JSON.stringify([arr.length-1,arr[0].length-2])){
+            queue.push([dest.dest, totalSteps])
+        }
+    }
+    //console.log(queue)
+    //console.log(nodeDistances)
+
+    count++
 }
+
+console.log("answer", nodeDistances.get(JSON.stringify([arr.length-1,arr[0].length-2])))
